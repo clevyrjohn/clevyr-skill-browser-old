0696954d93e04cd3016e99283e3812e69af2901f
John Carmack <john.carmack@clevyr.com>
2022-07-07 13:32:57 -0700

    nothing yet, slow going

--- src/components/TheDashboard.vue
+++ src/components/TheDashboard.vue
@@ -1,3 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useGapi } from 'vue-gapi'
+
+
+const gapi = useGapi()
+
+function login() {
+    gapi.login().then(({ currentUser, gapi, hasGrantedScopes }) => {
+    console.log({ currentUser, gapi, hasGrantedScopes })
+    })
+}
+
+login();
+</script>
 
 <template>Hello</template>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
+import VueGapi from 'vue-gapi'
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
 app.use(router);
+
+console.log()
+
+app.use(VueGapi, {
+    apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+    clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    scope: 'https://www.googleapis.com/auth/spreadsheets',
+  })
 
 app.mount("#app");
