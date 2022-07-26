b1c062627b9aaebea43bf3ea4256af2f0ee4ca31
John Carmack <john.carmack@clevyr.com>
2022-07-26 06:28:39 -0700

    saving place before trying a new approach

--- src/components/TheDashboard.vue
+++ src/components/TheDashboard.vue
@@ -1,16 +1,5 @@
 <script setup lang="ts">
-import { useGapi } from 'vue-gapi'
 
-
-const gapi = useGapi()
-
-function login() {
-    gapi.login().then(({ currentUser, gapi, hasGrantedScopes }) => {
-    console.log({ currentUser, gapi, hasGrantedScopes })
-    })
-}
-
-login();
 </script>
 
 <template>Hello</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import VueGapi from 'vue-gapi'
 import App from "./App.vue";
 import router from "./router";
 
@@ -8,12 +7,5 @@ const app = createApp(App);
 app.use(router);
 
 console.log()
-
-app.use(VueGapi, {
-    apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
-    clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    scope: 'https://www.googleapis.com/auth/spreadsheets',
-  })
 
 app.mount("#app");
